bac1f1a16b2efe988706d91ca0dbdd632daf06c4
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-05 09:42:32 +0600

    factorial

---
+++ factorial.js
@@ -0,0 +1,17 @@
+// 1 taka joto sunka dibo er gunfol.
+/*
+1 != 1
+2! = 2 * 1
+3! = 3 * 2 * 1
+4! = 4 * 3 * 2 * 1
+5! = 5 * 4 * 3 * 2 * 1
+*/
+function factorial(number) {
+    let result = 1;
+    for (let i = 1; i <= number; i++) {
+        result = result * i;
+    }
+    return result;
+}
+const result = factorial(7);
+console.log(result);
